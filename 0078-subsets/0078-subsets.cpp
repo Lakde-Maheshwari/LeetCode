5f0435a5e5ac2aa521be082b5485ac8ce7719378
Lakde Maheshwari <153929361+Lakde-Maheshwari@users.noreply.github.com>
2024-03-26 10:07:51 +0530

    Time: 2 ms (54.94%), Space: 11 MB (43.88%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void Subseq(vector<int> &arr,int index,int n,vector<vector<int >>&ans,vector<int> temp){
+    void Subseq(vector<int> &arr,int index,int n,vector<vector<int >>&ans,vector<int> &temp){
     if(index == n){
         ans.push_back(temp);
         return;
@@ -11,6 +11,7 @@ public:
     // yes ke case mein pehle wo number ko add kro fir uskko call lagao
     temp.push_back(arr[index]);
     Subseq(arr,index+1,n,ans,temp);
+        temp.pop_back();
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
