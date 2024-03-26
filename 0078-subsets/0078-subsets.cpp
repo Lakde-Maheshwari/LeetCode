43c6a3bb14a28da1aa93712e8261dcf14ed0476c
Lakde Maheshwari <153929361+Lakde-Maheshwari@users.noreply.github.com>
2024-03-26 10:03:26 +0530

    Time: 0 ms (100.00%), Space: 13.8 MB (34.37%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void Subseq(vector<int> &arr,int index,int n,vector<vector<int >>&ans,vector<int> temp){
+    if(index == n){
+        ans.push_back(temp);
+        return;
+    }
+    // ek to hum us element ko lenge vrna nhi lenge do hi ho skta hai
+    // no ke case mein ye aayega
+    Subseq(arr,index+1,n,ans,temp);
+    // yes ke case mein pehle wo number ko add kro fir uskko call lagao
+    temp.push_back(arr[index]);
+    Subseq(arr,index+1,n,ans,temp);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>>ans;
+        vector<int> temp;
+        Subseq(nums,0,n,ans,temp);
+        return ans;
+    }
+};
